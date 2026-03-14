6319eace86b5c3804dba517334ae75c7a78df1b1
Tulsi Mandani <tulsi123@gmail.com>
2026-03-14 17:12:55 +0530

    Initial commit

--- 2/1.js
+++
@@ -1,9 +0,0 @@
-// Get elements
-const button = document.getElementById("btn");
-const title = document.getElementById("title");
-
-// Add click event
-button.addEventListener("click", () => {
-    title.textContent = "You clicked the button!";
-    title.style.color = "blue";
-});
